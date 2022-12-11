ff40ebd1f9b7e51fcd81f7eb9b288c048481a501
sonde756 <edsonrafael756@gmail.com>
2022-12-11 20:34:08 -0300

    Menu interativo construido

--- Program.cs
+++ Program.cs
@@ -1,7 +1,46 @@
 ﻿using Projeto.Models;
 
 
+string opcao;
+bool exibirMenu = true;
 
+while(exibirMenu)
+{
+     Console.WriteLine("Digite sua opção");
+     Console.WriteLine("1- Cadastra clietnte");
+     Console.WriteLine("2- Buscar cliente");
+     Console.WriteLine("3- Apagar cliente");
+     Console.WriteLine("4- Encerrar");
+
+
+     opcao = Console.ReadLine();
+
+     switch(opcao)
+     {
+          case "1":
+               Console.WriteLine("Cadastrado de cliente");
+               break;
+          case "2":
+               Console.WriteLine("Buscar cliente");
+               break;
+          case "3":
+               Console.WriteLine("Apagar cliente");
+               break;
+          case "4":
+               Console.WriteLine("Encerrar");
+               exibirMenu = false;
+               //Environment.Exit(0);
+               break;
+
+          default:
+          Console.WriteLine("Opção invalida");
+               break;
+     }         
+
+     
+}     
+
+Console.WriteLine("Programa se encerrou");
 
 
 
